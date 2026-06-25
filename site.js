997f9d94fa5d05965119e31b4dd887a46e998a98
tobiashales <brothertobias14@gmail.com>
2026-06-24 20:29:17 -0600

    Stabilize mobile viewport sizing

--- site.js
+++
@@ -1,7 +0,0 @@
-function setBigTimePixelsViewportHeight() {
-  document.documentElement.style.setProperty("--btp-viewport-height", `${window.innerHeight}px`);
-}
-
-setBigTimePixelsViewportHeight();
-window.addEventListener("resize", setBigTimePixelsViewportHeight, { passive: true });
-window.addEventListener("orientationchange", setBigTimePixelsViewportHeight, { passive: true });
